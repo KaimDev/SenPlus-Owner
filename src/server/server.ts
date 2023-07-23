3fb03c54c5a81a1610c7aa5be3079c710b297c09
KaimDev <johanedfb@gmail.com>
2023-07-23 01:45:09 -0600

    Implementing authentication for the http request
    src/
      * server/server.ts: Implementing authentication by bearer token

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,25 @@
 import http from "http";
 import { MessageDTO } from "./dto/message.dto";
 import { sendMessage } from "../utils/send-message";
+require("dotenv").config();
 
 export function Server() {
   const port = 3000;
+  const bearerToken = process.env.BEARER_TOKEN;
 
   const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
     try {
       if (req.method === "POST" && req.url === "/message") {
+        const authorizationHeader = req.headers["authorization"];
+        const tokenFromHeader = authorizationHeader ? authorizationHeader.replace("Bearer ", "") : undefined;
+
+        if (tokenFromHeader !== bearerToken) {
+          // Token from header does not match the expected bearerToken
+          res.writeHead(401, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ success: false, error: "Unauthorized" }));
+          return;
+        }
+
         let body = "";
 
         req.on("data", (chunk) => {
@@ -37,6 +49,7 @@ export function Server() {
         });
       } else {
         res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Endpoint not found");
       }
     } catch (error: any) {
       console.error("Server Error:", error.message);
