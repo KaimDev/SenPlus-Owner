48ea587797e427e152ce7f73f270a98a6751e394
KaimDev <johanedfb@gmail.com>
2023-08-02 08:51:43 -0600

    Request for create users implements axios
      * package.json
    
    src/
      * utils/create-user.ts

--- src/utils/create-user.ts
+++ src/utils/create-user.ts
@@ -1,6 +1,6 @@
 import { bot } from "..";
 import { User } from "../models/user.interface";
-import { sendLog } from "../owner/send-log";
+import axios from "axios";
 require("dotenv").config();
 
 export async function createUser({ chatId, username, role = "USER" }: User) {
@@ -14,24 +14,25 @@ export async function createUser({ chatId, username, role = "USER" }: User) {
     role,
   };
 
-  fetch(url + endpoint, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      console.log(response.status, response.statusText);
-      if (response.status === 201)
-        bot.sendMessage(
-          ownerChat,
-          `NEW USER WAS CREATED\n ${JSON.stringify(data, null, 2)}`
-        );
-      else if (response.status === 409)
-        bot.sendMessage(ownerChat, JSON.stringify(`ERROR ${response.status} ${response.statusText}`, null, 2));
-    })
-    .catch((error) => {
-      console.error(error);
+  try {
+    const response = await axios.post(url + endpoint, data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    console.log(response.status, response.statusText);
+    if (response.status === 201) {
+      bot.sendMessage(
+        ownerChat,
+        `NEW USER WAS CREATED\n ${JSON.stringify(data, null, 2)}`
+      );
+    }
+  } catch (error: any) {
+    console.error(JSON.stringify(error.message, null, 2));
+    bot.sendMessage(
+      ownerChat,
+      JSON.stringify(`REGISTER ERROR: ${error.message}`, null, 2)
+    );
+  }
 }
